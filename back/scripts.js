aeaa64a29157f738863a3f51f763303156bc08b2
creador <isidrotorresadriandavid@gmail.com>
2024-06-03 13:37:32 -0500

    reestructurar la parte del backend para que sea visiblemente mas facil de leer

--- back/scripts.js
+++ back/scripts.js
@@ -1,101 +1,116 @@
-// se definen las referencias a los contenedores
 const operadores = document.querySelector(".operadores");
 const numeros = document.querySelector(".numeros");
 const controles = document.querySelector(".controles");
-
-//referencia de la pantalla
 const output = document.getElementById('output');
 
-// se crean objetos que contegan la informacion de los botones
-const operador = {
-    'suma' : "+",
-    'resta' : "-",
-    'multiplicacion' : "*",
-    'division' : "/",
-}
-const numero = {
-    'button1': "1",
-    'button2': "2",
-    'button3': "3",
-    'button4': "4",
-    'button5': "5",
-    'button6': "6",
-    'button7': "7",
-    'button8': "8",
-    'button9': "9",
-    'button0': "0",
-    'punto': ".",
-}
-const control = {
-    'borrar' : "Clear",
-    'resultado': "=",
-    'deshacer': "|<=",
+//evitamos con esta variable que se digite un numero con varios "." ej: (2 * 1.2.3) 
+var esPunto;
+
+//retorna el ultimo caracter que contiene la pantalla
+const ultimoCaracter = () =>{
+    return output.textContent.charAt(output.textContent.length-1);
 }
 
-// funcion que crea los botones dependiendo del objeto y el contenedor
-// que se especifique
-const asignarBotones = (contenedor,objeto) => {
-    for(let i in objeto){
-        // creamos el boton con los atributos class y value
-        const boton = document.createElement('button');
-        boton.setAttribute('class',i);
-        boton.setAttribute('value',objeto[i]);
-        boton.textContent = objeto[i];
-        contenedor.appendChild(boton);
+// visualiza en pantalla el elemento que se le proporcione
+const visualizar = elemento =>{
+    output.textContent += elemento;
+}
+
+//retorna true si hay 2 signos de seguido, de lo contrario retorna false
+const esSigno = ()=>{
+    const caracter = ultimoCaracter();
+    let respuesta = false;
+    if(caracter ==='+' || caracter ==='-' || caracter ==='*' || caracter ==='/' || caracter ==='.'){
+        respuesta = true;
+    }
+    return respuesta;
+}
+
+//valida que no se inicie con un signo y que no se puede digitar 2 signos de seguido
+const visualizarSigno = signo =>{
+    if(output.textContent === ''){
+        alert("no puedes iniciar la expresion con un signo");
+    }else{
+        if(esSigno()){
+            alert("verifique su expresion antes de proseguir");
+        }else{
+            if(signo === '.' && esPunto=== true){
+                alert('no puedes colocar un numero con varios puntos');
+            }else{
+                visualizar(signo);
+                if(signo==='.'){
+                    esPunto = true;
+                }else{
+                    esPunto = false;
+                }
+            }
+        }
     }
 }
 
-//funcion que asigna un evento escucha tipo click para mostrar en pantalla
-const mostrarEnPantalla = (objeto)=>{
-    for(let i in objeto){
-        document.querySelector('.'+i).addEventListener('click',()=>{
-            output.textContent += objeto[i];
-        });
-    }
+//limpia lo que contega la pantalla
+const limpiar = () =>{
+    output.textContent = '';
 }
 
-// asignamos la funcion que limpia la pantalla
-const limpiarPantalla = () => {
-    output.textContent = "";
-}
-
-// asignamos la funcion de eliminar ultimo elemento de la pantalla
-const desHacer = () => {
+// elimina el ultimo elemento de la pantalla
+const deshacer = () =>{
     output.textContent = output.textContent.slice(0,-1);
 }
 
-const verificador = () => {
-    for(let i in operador){
-        let boton = document.querySelector("."+i);
-        boton.addEventListener('click',()=>{
-            //impedimos que haya 2 operadores de seguido y que inice con * o /
-            let ultimoCaracter = output.textContent.charAt(output.textContent.length-2);
-            let caracterActual = output.textContent.charAt(output.textContent.length-1);
-            for(let j in operador){
-                if(ultimoCaracter==="" && caracterActual === '*' || ultimoCaracter==="" && caracterActual === '/'){
-                    alert("debe seleccionar un numero antes de usar este operador");
-                    desHacer();
-                    break;
-                }else if(ultimoCaracter===operador[j]){//encontró 2 operadores de seguido
-                    alert("no puede haber 2 operadores de seguido");
-                    desHacer();
-                }
-            }
-        })
+// inicia los procesos para obtener el resultado matematico
+const respuesta = () =>{
+    if(esSigno()){
+        alert("no puedes terminar la expresion con un signo");
+    }else{
+        alert("aun no se ha finalizado esta funcion, proximamente...")
+        limpiar();
     }
 }
-//solicitamos que se visualicen los botones en la calculadora y en pantalla
-asignarBotones(operadores,operador);
-mostrarEnPantalla(operador);
-asignarBotones(numeros,numero);
-mostrarEnPantalla(numero);
-asignarBotones(controles,control);
-verificador();
-document.querySelector('.borrar').addEventListener('click',limpiarPantalla);
-document.querySelector('.deshacer').addEventListener('click',desHacer);
 
+//objeto que contiene cada boton y sus caracteristicas;
+const objetos = {
+    // class  : [contenedor value onclick()],
+    'button1' : ['numeros','1','visualizar("1")'],
+    'button2' : ['numeros','2','visualizar("2")'],
+    'button3' : ['numeros','3','visualizar("3")'],
+    'button4' : ['numeros','4','visualizar("4")'],
+    'button5' : ['numeros','5','visualizar("5")'],
+    'button6' : ['numeros','6','visualizar("6")'],
+    'button7' : ['numeros','7','visualizar("7")'],
+    'button8' : ['numeros','8','visualizar("8")'],
+    'button9' : ['numeros','9','visualizar("9")'],
+    'button0' : ['numeros','0','visualizar("0")'],
+    'punto' : ['numeros','.','visualizarSigno(".")'],
+    'suma' : ['operadores','+','visualizarSigno("+")'],
+    'resta' : ['operadores','-','visualizarSigno("-")'],
+    'multiplicacion' : ['operadores','*','visualizarSigno("*")'],
+    'division' : ['operadores','/','visualizarSigno("/")'],
+    'deshacer': ['controles','<=','deshacer()'],
+    'limpiar': ['controles','C','limpiar()'],
+    'repuesta' : ['controles','=','respuesta()']
+}
 
-// creamos condiciones logicas:
-// 1- no puede haber 2 operadores ni puntos de seguido
-// 2- se puede iniciar con los operadores - y + si no hay un numero antes pero no se puede iniciar con * o /
-// 3- caundo se presione en resultado primero debe realizar la multiplicacion y la division y despues la suma y la resta 
+//constructor de los botones;
+const iniciar = () =>{
+    for(let i in objetos){
+        const button = document.createElement('button');
+        button.setAttribute('class',i);
+        button.setAttribute('value',objetos[i][1]);
+        button.setAttribute('onclick',objetos[i][2]);
+        button.textContent = objetos[i][1];
+        var contenedor = objetos[i][0];
+        switch(contenedor){
+            case 'numeros':
+                numeros.appendChild(button);
+                break;
+            case 'controles':
+                controles.appendChild(button);
+                break;
+            case 'operadores':
+                operadores.appendChild(button);
+                break;
+        }
+    }
+}
+iniciar();
